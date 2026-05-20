124ba450e47dbe4dd44c469ad86497820da6fe61
arun980798 <arunrajput88176894@gmail.com>
2026-05-21 01:57:26 +0530

    feat: integrate ImageKit for image upload and configure environment variables

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,10 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-
 const config = {
   mongodb: process.env.mongodb,
-  jwtsecret: process.env.jwt_secret
-}
+
+  jwtsecret: process.env.jwt_secret,
+
+  publicKey: process.env.imagekit_public_key,
+
+  privateKey: process.env.imagekit_private_key,
+
+  urlEndpoint: process.env.imagekit_url_endpoint,
+};
 
 export default config;
