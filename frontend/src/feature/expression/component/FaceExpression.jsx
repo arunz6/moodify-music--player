17b8fe9b17f72f086cd7dbe0bb3d3d3c7d6384fa
arun980798 <arunrajput88176894@gmail.com>
2026-05-23 00:58:52 +0530

    feat: refactor FaceExpression component layout and improve cleanup logic

--- frontend/src/feature/expression/component/FaceExpression.jsx
+++ frontend/src/feature/expression/component/FaceExpression.jsx
@@ -1,39 +1,46 @@
 import { useEffect, useRef, useState } from "react";
-import { detect,init } from "../utils/util";
-
+import { detect, init } from "../utils/util";
 
 export default function FaceExpression() {
-    const videoRef = useRef(null);
-    const landmarkerRef = useRef(null);
-    const streamRef = useRef(null);
+  const videoRef = useRef(null);
+  const landmarkerRef = useRef(null);
+  const streamRef = useRef(null);
 
-    const [ expression, setExpression ] = useState("Detecting...");
+  const [expression, setExpression] = useState("Detecting...");
 
-    useEffect(() => {
-        init({landmarkerRef,videoRef,streamRef});
+  useEffect(() => {
+    init({ landmarkerRef, videoRef, streamRef });
 
-        return () => {
-            if (landmarkerRef.current) {
-                landmarkerRef.current.close();
-            }
+    return () => {
+      if (landmarkerRef.current) {
+        landmarkerRef.current.close();
+      }
 
-            if (videoRef.current?.srcObject) {
-                videoRef.current.srcObject
-                    .getTracks()
-                    .forEach((track) => track.stop());
-            }
-        };
-    }, []);
+      if (videoRef.current?.srcObject) {
+        videoRef.current.srcObject.getTracks().forEach((track) => track.stop());
+      }
+    };
+  }, []);
 
-    return (
-        <div style={{ textAlign: "center" }}>
-            <video
-                ref={videoRef}
-                style={{ width: "400px", borderRadius: "12px" }}
-                playsInline
-            />
-            <h2>{expression}</h2>
-            <button onClick={()=>{detect({landmarkerRef,videoRef,setExpression})}} >Detect expression</button>
-        </div>
-    );
+  return (
+    <div className="flex flex-col justify-center items-center  space-y-4">
+      <video
+        className=" border-2 border-gray-300 w-full rounded-2xl shadow-md "
+        ref={videoRef}
+        playsInline
+      />
+      <div className="bootam flex  justify-between w-full items-center  space-y-4">
+        {" "}
+        <h2 className=" text-3xl text-white font-serif ">{expression}</h2>
+        <button
+          className=" bg-blue-700 bg-slate-600 px-4 py-3 rounded-xl text-teal-400 font-medium hover:scale-101 active:scale-90  transition-all duration-200"
+          onClick={() => {
+            detect({ landmarkerRef, videoRef, setExpression });
+          }}
+        >
+          Detect expression
+        </button>
+      </div>
+    </div>
+  );
 }
--- frontend/src/feature/home/pages/Home.jsx
+++ frontend/src/feature/home/pages/Home.jsx
@@ -5,9 +5,13 @@ import Player from "../components/player.jsx";
 const Home = () => {
   return (
     <>
-      <div className="space-y-10 px-4 py-6">
-        <FaceExpression />
-        <Player />
+      <div className="space-y-10 h-full w-full flex bg-gray-900 gap-5 justify-around px-4 py-6">
+        <div className="faceexpression w-1/2 ">
+          <FaceExpression />
+        </div>
+        <div className="player w-1/2 ">
+          <Player />
+        </div>
       </div>
     </>
   );
